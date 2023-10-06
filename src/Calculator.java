36aae6b700c103f88149c8f2bf37552191e46794
unknown <F219517@NUCFD.EDU.LOCAL>
2023-10-06 09:59:56 +0500

    Created Calculator class with add and subtract function only without creating main function

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+
+	public int add() {
+		
+		int a = 10, b = 15;
+		int result = a + b;
+		return result;
+	}
+	
+	public int subtract() {
+		
+		int a = 10, b = 5;
+		int result = a - b;
+		return result;
+	}
+}
